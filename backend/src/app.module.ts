762f2a2674c5d9722408c70f19984a8706f7731a
Jefferson <jefferson.swanto@gmail.com>
2026-07-10 14:01:52 +0700

    init: validator-transform , config, swagger setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestModule } from './test/test.module';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
-  imports: [TestModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        // SUPABASE_URL: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+        //nanti lagi kalo ada yg wajib
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
