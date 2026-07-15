c3daff770482dc96259a310de7886f4b6b4dc750
Jefferson <jefferson.swanto@gmail.com>
2026-07-15 16:17:36 +0700

    feat: enabling cors to fe port

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -16,7 +16,7 @@ import * as Joi from 'joi';
         SUPABASE_SECRET_KEY: Joi.string().required(),
         INV_JWT_SECRET: Joi.string().default('2d'),
         INV_JWT_EXPIRES_IN: Joi.string().required(),
-        PORT: Joi.number().default(3000),
+        PORT: Joi.number().default(3001),
         FRONTEND_URL: Joi.string().default('http://localhost:3001')
         //nanti lagi kalo ada yg wajib
       }),
